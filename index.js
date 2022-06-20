444bbc4a81d956409409c6aeb391dae7914f84d5
vics <vics@ebay.com>
2022-06-20 17:11:29 -0400

    dev: interfaces

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const app = require('express')();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.get('/update', require('./actions/update'));
+app.get('/get', require('./actions/get'));
+app.get('/poll', require('./actions/poll'));
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
